cb3a89a609166fb35c00e3dc4042b519f326663c
danilbelyaev1 <danilbelyaev1@bk.ru>
2021-09-02 13:04:38 +0500

    фыва

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// 'use strict';
+// window.addEventListener('DOMContentLoaded', function(){
+//     document.querySelectorAll('input').forEach(item => {
+//     item.addEventListener('focus', function(){
+//         console.log('qweqwe');
+//         this.classList.remove('.value');
+//     });
+// });
+// });
